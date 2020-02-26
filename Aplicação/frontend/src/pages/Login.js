d3452094f25f2017d48a10dba1922d4cd7127559
Henrique <henrique.matos@bandtec.com.br>
2020-02-26 14:25:16 -0300

    tela login

--- Aplicação/frontend/src/pages/Login.js
+++ Aplicação/frontend/src/pages/Login.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import "./Login.css";
 import logo from '../logo.svg';
+import SlideDown from './React-slidedown.css'
+ 
+export function SlideDropDown(props) {
+  return (
+    <SlideDown className={'login-container form'} transitionOnAppear>
+      {props.open ? props.children : null}
+    </SlideDown>
+  )
+}
 
 export default function Login(){
     return(
+    
+       
+     
         <div className="login-container">
             <form>
         <img src={logo} alt="Hunter"></img>
-        <input placeholder="Digite seu login"> </input>
-        <button type ="submit"> Enviar</button>
+        <input placeholder="Digite seu login"/>
+        <input type="password" placeholder="Digite Sua Senha"/>
+        <a href=""> Esqueceu sua senha?</a>
+        <div>
+        <button className="login container btCad" type ="submit"> Cadastrar</button>
+        <button className="login container btLog" type ="submit"> Enviar</button>
+       
+        </div>
             </form>
         </div>    
         
