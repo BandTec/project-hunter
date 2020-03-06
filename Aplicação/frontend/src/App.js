e4685f3847a5961a2086c939cd554ad29dd21e88
Henrique <henrique.matos@bandtec.com.br>
2020-03-06 11:50:56 -0300

    readicionando front-end

---
+++ Aplicação/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import InstructorApp from './component/InstructorApp';
+import Login from "./pages/Login"
+import Cadastro from "./pages/Cadastro"
+
+function App() {
+  return (
+   <InstructorApp/>
+  );
+}
+
+export default App;
---
+++ Aplicação/frontend/src/pages/Cadastro.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import "./Cadastro.css";
+import logo from '../logo.svg';
+import Login from './Login';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+class Cadastro extends Component {
+    constructor(args) {
+        super(args)
+        this.state = {
+            name: '',
+            region: '',
+            email: '',
+            password: '',
+            redirect: false
+        }
+    }
+
+    onChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    chamaLogin = () => {
+        this.setState({
+          redirect: true 
+        })
+    }
+    render() {
+
+        if (this.state.redirect) {
+        
+            return <Router><Redirect to="/Login" /></Router>
+         
+        }
+        else {
+            return (
+                <div className="cadastro-container">
+                    <form>
+                        <img src={logo} alt="Hunter"></img>
+                        <input placeholder="Digite seu nome" />
+                        <input placeholder="Digite seu e-mail" />
+                        <select id="regiao">
+                            <option value="sp">São Paulo</option>
+                            <option value="rj">Rio de Janeiro</option>
+                        </select>
+                        <input type="password" placeholder="Digite Sua Senha" />
+                        <input type="password" placeholder="Confirmar Senha" />
+                        <Router>
+                        <div>
+                            <button className="login container btVoltar" type="submit"
+                                onClick={() => this.chamaLogin()}> Voltar</button>
+
+                            <button className="login container btCad" onClick="Cadastrar()" type="submit"> Cadastrar</button>
+
+                        </div>
+                        </Router>
+
+
+                    </form>
+                </div>
+
+            );
+
+        }
+    }
+}
+export default Cadastro;
