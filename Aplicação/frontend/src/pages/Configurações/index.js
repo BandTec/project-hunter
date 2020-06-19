d8d943f0b0b56d8c58f80a16eefd13800cfa8a73
Henriqe Matos <henriquee.matos@hotmail.com>
2020-06-19 19:49:22 -0300

    corrigindo tela configuracoes

--- Aplicação/frontend/src/pages/Configurações/index.js
+++ Aplicação/frontend/src/pages/Configurações/index.js
@@ -121,33 +121,33 @@ const useStyles2 = makeStyles((theme) => ({
 
 export default function Configurações() {
 
-    const [thumbnail, setThumbnail] = useState(null);
-    const [nome, setNome] = useState('');
-    const [usuario, setUsuario] = useState('');
-    const [cpf, setCpf] = useState('');
-    const [email, setEmail] = useState('');
-    const [telefone, setTelefone] = useState('');
-    const [idJogo, setIdJogo] = useState('');
-    const [idPosicao, setIdPosicao] = useState('');
-    const [senha, setSenha] = useState('');
-    const [pesquisa, setPesquisa] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+  const [nome, setNome] = useState('');
+  const [usuario, setUsuario] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [idJogo, setIdJogo] = useState('');
+  const [idPosicao, setIdPosicao] = useState('');
+  const [senha, setSenha] = useState('');
+  const [pesquisa, setPesquisa] = useState("");
 
   console.log(localStorage);
 
   // Botão Usuário 
 
-  async function handlePesquisa () {
+  async function handlePesquisa() {
     localStorage.setItem('pesquisa', pesquisa);
     history.push("/busca", pesquisa);
   }
 
   const preview = useMemo(() => { return thumbnail ? URL.createObjectURL(thumbnail) : null },
-  thumbnail);
-function onChange(e) {
-  this.setState({
+    thumbnail);
+  function onChange(e) {
+    this.setState({
       [e.target.name]: e.target.value
-  })
-}
+    })
+  }
 
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
@@ -192,42 +192,42 @@ function onChange(e) {
   React.useEffect(() => {
     async function dadosPerfil() {
       const response = await api.get(`/gamer/${emailSelect}/`); //{
-    
-   let dados = response.data;
 
-   let temp = [];
+      let dados = response.data;
 
-   dados.forEach( item => {
-     temp.push(
-       criaDados(
-         item.nome,
-         item.usuario,
-         item.cpf,
-         item.email,
-         item.senha,
-         item.telefone
-       )
-     );
-   });
-   
-   setNome(temp[0].nome);
-   setUsuario(temp[0].usuario);
-   setCpf(temp[0].cpf);
-   setEmail(temp[0].email);
-   setSenha(temp[0].senha);
-   setTelefone(temp[0].telefone);
-   localStorage.setItem('nome', temp[0].nome);
-   localStorage.setItem('usuario', temp[0].usuario);
-   localStorage.setItem('cpf', temp[0].cpf);
-   localStorage.setItem('email', temp[0].email);
-   localStorage.setItem('senha', temp[0].senha);
-   localStorage.setItem('telefone', temp[0].telefone);
- }
- dadosPerfil();
-}, []);
+      let temp = [];
 
-function criaDados(nome, usuario, cpf, email, senha, telefone){
-    return {nome, usuario, cpf, email, senha, telefone}
+      dados.forEach(item => {
+        temp.push(
+          criaDados(
+            item.nome,
+            item.usuario,
+            item.cpf,
+            item.email,
+            item.senha,
+            item.telefone
+          )
+        );
+      });
+
+      setNome(temp[0].nome);
+      setUsuario(temp[0].usuario);
+      setCpf(temp[0].cpf);
+      setEmail(temp[0].email);
+      setSenha(temp[0].senha);
+      setTelefone(temp[0].telefone);
+      localStorage.setItem('nome', temp[0].nome);
+      localStorage.setItem('usuario', temp[0].usuario);
+      localStorage.setItem('cpf', temp[0].cpf);
+      localStorage.setItem('email', temp[0].email);
+      localStorage.setItem('senha', temp[0].senha);
+      localStorage.setItem('telefone', temp[0].telefone);
+    }
+    dadosPerfil();
+  }, []);
+
+  function criaDados(nome, usuario, cpf, email, senha, telefone) {
+    return { nome, usuario, cpf, email, senha, telefone }
   }
 
   function handleProfile() {
@@ -261,80 +261,82 @@ function criaDados(nome, usuario, cpf, email, senha, telefone){
   function handleHome() {
 
     history.push('/home');
-}
+  }
 
-const id = localStorage.getItem('idGamer');
+  const id = localStorage.getItem('idGamer');
+  console.log(id);
 
   async function handleSignUp(e) {
     e.preventDefault();
     if (nome === "" || cpf === "" || email === "" || senha === "" || usuario === "" || telefone === "") {
-        alert('Por favor preencha todos os campos!');
+      alert('Por favor preencha todos os campos!');
     } else {
 
-        const data = {
-            nome,
-            cpf,
-            email,
-            telefone,
-            senha
-        };
-
-        try {
-            const response = await api.put(`/gamer/${id}}`, data); 
-            //alert(`Seu ID de Acesso ${response.data.id}`);
-            if (response.status === 201){
-              handleSignUp2();
-              }else{
-                  alert('Erro na atualização de seus dados, tente novamente!');
-              }
-          } catch (err) {
-              alert('Erro na atualização de seus dados, tente novamente!');
-          }
-      }
-  }
-
-async function handleSignUp2() {
-  if (idJogo === "" && idPosicao === "") {
-      alert('Dados alterados com sucesso!');
-  } 
-  else if (idJogo === "" || idPosicao === ""){
-    alert('Preencha seu jogo e sua posição!')
-  }
-  else {
-
-      const data2 = {
-          'idJogo': {
-              'idJogo': idJogo
-            },
-            'idPosicao': {
-              'idPosicao': idPosicao
-            }
-          };
+      const data = {
+        nome,
+        cpf,
+        email,
+        telefone,
+        senha
+      };
 
       try {
-          const response = await api.post(`/gamerinfo/${email}`, data2);
-          //alert(`Seu ID de Acesso ${response.data.id}`);
-          if (response.status === 201){
-            alert('Dados alterados com sucesso!');
-            history.push('/home')
-          }else{
-              alert('Erro no cadastro de seu jogo e/ou posição, tente novamente!');
-          }
+        const response = await api.put(`/gamer/${id}`, data);
+        //alert(`Seu ID de Acesso ${response.data.id}`);
+        
+        if (response.status === 200) {
+          handleSignUp2();
+        } else {
+          alert('Erro na atualização de seus dados, tente novamente!');
+        }
       } catch (err) {
-          alert('Erro no cadastro de seu jogo e/ou posição, tente novamente!');
+        alert('Erro na atualização de seus dados ou conexão com a aplicação, tente novamente!');
       }
+    }
   }
-}
+
+  async function handleSignUp2() {
+    if (idJogo === "" && idPosicao === "") {
+      alert('Dados alterados com sucesso!');
+    }
+    else if (idJogo === "" || idPosicao === "") {
+      alert('Preencha seu jogo e sua posição!')
+    }
+    else {
+
+      const data2 = {
+        'idJogo': {
+          'idJogo': idJogo
+        },
+        'idPosicao': {
+          'idPosicao': idPosicao
+        }
+      };
+
+      try {
+        const response = await api.post(`/gamerinfo/${email}`, data2);
+        //alert(`Seu ID de Acesso ${response.data.id}`);
+        if (response.status === 201) {
+          alert('Dados alterados com sucesso!');
+          history.push('/home')
+        } else {
+          alert('Erro no cadastro de seu jogo e/ou posição, tente novamente!');
+        }
+      } catch (err) {
+        alert('Erro no cadastro de seu jogo e/ou posição, tente novamente!');
+      }
+    }
+  }
 
   return (
     <div className="configuracao-container">
 
       <header>
-        <img src={Logo} alt="HunterProject"  onClick={handleHome}></img>
+        <img src={Logo} alt="HunterProject" onClick={handleHome}></img>
 
 
         <div className="input-pesquisa">
-        <input
+          <input
             type="text"
             placeholder="Busque por jogos ou equipes..."
             onChange={(e) => setPesquisa(e.target.value)}
@@ -364,10 +366,10 @@ async function handleSignUp2() {
               <Paper className={classes.root} >
                 <ClickAwayListener onClickAway={handleClose}>
                   <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
-                  <MenuItem onClick={handleProfile}>Perfil</MenuItem>
-                                    <MenuItem onClick={handleEquipe}>Minhas Equipes</MenuItem>
-                                    <MenuItem onClick={handleConfig}>Configurações</MenuItem>
-                                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                    <MenuItem onClick={handleProfile}>Perfil</MenuItem>
+                    <MenuItem onClick={handleEquipe}>Minhas Equipes</MenuItem>
+                    <MenuItem onClick={handleConfig}>Configurações</MenuItem>
+                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
                   </MenuList>
                 </ClickAwayListener>
               </Paper>
@@ -375,72 +377,75 @@ async function handleSignUp2() {
           )}
         </Popper>
       </header>
-<div className="formulario">
-      <form onSubmit={handleSignUp}>
-                <label id="thumbnail"
-                    style={{ backgroundImage: `url(${preview})` }}
-                    className={thumbnail ? 'has-thumbnail' : ''}
-                >
-                   {/* <img className="profile-pic" src = {require(`../../assets/${localStorage.getItem('nome')}-icon.jpg`)} alt="Foto de Perfil"></img> */}
-                    <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
-                    <img src={camera} alt="Select your photo"></img>
+      <div className="formulario">
+        <form onSubmit={handleSignUp}>
+          <label id="thumbnail"
+            style={{ backgroundImage: `url(${preview})` }}
+            className={thumbnail ? 'has-thumbnail' : ''}
+          >
+            {/* <img className="profile-pic" src = {require(`../../assets/${localStorage.getItem('nome')}-icon.jpg`)} alt="Foto de Perfil"></img> */}
+            <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
+            <img src={camera} alt="Select your photo"></img>
 
-                </label>
-                <rows>
-                <br></br>
-                <div className = "campos3">
-                <div className="campos">
-                <p className = "campo">Nome:</p>
+          </label>
+          <rows>
+            <br></br>
+            <div className="campos3">
+              <div className="campos">
+                <p className="campo">Nome:</p>
                 <input value={nome} onChange={e => setNome(e.target.value)} />
-                <p className = "campo">CPF:</p>
+                <p className="campo">CPF:</p>
                 <input value={cpf} onChange={e => setCpf(e.target.value)} />
-                <p className = "campo">Email:</p>
+                <p className="campo">Email:</p>
                 <input value={email} onChange={e => setEmail(e.target.value)} />
-                <p className = "campo">Jogo:</p>
+                <p className="campo">Jogo:</p>
                 <select onChange={e => setIdJogo(e.target.value)}>
-                <option value='0'>Selecione o jogo</option>
-                <option value='1'>Counter-Strike: Global Offensive</option>
-                <option value='2'>Valorant</option>
-                <option value='3'>League of Legends</option>
-                <option value='4'>Fortnite</option>
-                <option value='5'>DOTA 2</option>
-                <option value='6'>Call of Duty: Warzone</option>
-                <option value='7'>PlayerUnkown's Battlegrounds</option>
-                </select> 
-                </div>
-                <div className="campos2">
-                <p className = "campo">Usuário:</p>
-                <input value = {usuario} onChange={e => setUsuario(e.target.value)} />              
-                <p className = "campo">Telefone:</p>
-                <input value={telefone} onChange={e => setTelefone(e.target.value)} />                                          
-                <p className = "campo">Senha:</p>
-                <input type = "password" value={senha} onChange={e => setSenha(e.target.value)} />
-                <p className = "campo">Posição:</p>
-                <select onChange={e => setIdPosicao(e.target.value)}>
-                <option value='0'>Selecione a sua posição</option>
-                <option value='2'>Atirador</option>
-                <option value='3'>Suporte</option>
-                <option value='4'>Jungle</option>
-                <option value='5'>Top</option>
-                <option value='6'>Mid</option>
-                <option value='7'>Entry Fragger</option>
-                <option value='8'>Lurker</option>
-                <option value ='9'>Capitão</option>
-                <option value ='10'>Sniper</option>
+                  <option value='0'>Selecione o jogo</option>
+                  <option value='1'>Counter-Strike: Global Offensive</option>
+                  <option value='2'>Valorant</option>
+                  <option value='3'>League of Legends</option>
+                  <option value='4'>Fortnite</option>
+                  <option value='5'>DOTA 2</option>
+                  <option value='6'>Call of Duty: Warzone</option>
+                  <option value='7'>PlayerUnkown's Battlegrounds</option>
                 </select>
-                </div> 
-                </div>
-                </rows>
-                </form>
-
-                <Router>
-                  <center>
-                        <button className="configuracao container btn Cad"  type="submit">Salvar</button>
-                        </center>
-                </Router>
-
-                
+              </div>
+              <div className="campos2">
+                <p className="campo">Usuário:</p>
+                <input value={usuario} onChange={e => setUsuario(e.target.value)} />
+                <p className="campo">Telefone:</p>
+                <input value={telefone} onChange={e => setTelefone(e.target.value)} />
+                <p className="campo">Senha:</p>
+                <input type="password" value={senha} onChange={e => setSenha(e.target.value)} />
+                <p className="campo">Posição:</p>
+                <select onChange={e => setIdPosicao(e.target.value)}>
+                  <option value='0'>Selecione a sua posição</option>
+                  <option value='2'>Atirador</option>
+                  <option value='3'>Suporte</option>
+                  <option value='4'>Jungle</option>
+                  <option value='5'>Top</option>
+                  <option value='6'>Mid</option>
+                  <option value='7'>Entry Fragger</option>
+                  <option value='8'>Lurker</option>
+                  <option value='9'>Capitão</option>
+                  <option value='10'>Sniper</option>
+                </select>
+              </div>
             </div>
+          </rows>
+
+          <center>
+            <button className="configuracao container btn Cad" type="submit">Salvar</button>
+          </center>
+
+        </form>
+
+
+
+
+
+
+      </div>
     </div>
   );
 }
