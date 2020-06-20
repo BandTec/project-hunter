d98063ee6b0b78ad7352c77d517cc189cc82564b
Fernando <fe.c.correa10@gmail.com>
2020-06-19 22:15:51 -0300

    modal no alerta

--- Aplicação/frontend/src/pages/RecSenha/index.js
+++ Aplicação/frontend/src/pages/RecSenha/index.js
@@ -6,7 +6,26 @@ import { Link, useHistory } from 'react-router-dom';
 import api from '../../services/api';
 import { waitForElementToBeRemoved } from '@testing-library/react';
 
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 export default function Register() {
+
+  const [open, setOpen] = React.useState(false);
+  const [dados, setDados] = useState('wqeqwe');
+  
+  function handleOpenAlert (resposta) {
+    setDados(resposta);
+    setOpen(true);
+  };
+
+  const handleCloseAlert = () => {
+    setOpen(false);
+  };
 
     const [email, setEmail] = useState('');
 
@@ -19,9 +38,8 @@ export default function Register() {
     async function handlePassRecover(e) {
         e.preventDefault();
 
-    
     if (!email) {
-      alert("Por favor, digite seu e-mail para continuar!");
+      handleOpenAlert('Por favor, digite seu email.');
     } else {
       try {
         
@@ -30,16 +48,14 @@ export default function Register() {
           if (response.status === 200){
           
           localStorage.setItem('email', email);
-          
-          alert('As instruções de recuperação de senha enviadas para seu email!');
-
+            handleOpenAlert('As instruções de recuperação de senha enviadas para seu email!');
           history.push("/");
           
           }else{
-            alert("Email inválido");
+            handleOpenAlert('Email inválido');
           }
       } catch (err) {
-        alert("Email inválido");
+        handleOpenAlert('Email inválido');
         
       }
     }
@@ -51,15 +67,35 @@ export default function Register() {
                 <img src={Logo} alt="HunterProject" onClick = {handleVoltar}></img>
             </header>
             <div >
-                <form onSubmit={handlePassRecover}>
+                <form onSubmit = {handlePassRecover}>
                     <h1>Digite seu email:</h1>
                     <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-
                     <div>
                         <button className="btn env" type="submit">Enviar</button>
                     </div>
                 </form>
             </div>
+            <div>
+      <Dialog
+        open={open}
+        onClose={handleCloseAlert}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Erro</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {dados}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseAlert} color="primary" autoFocus>
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
         </div>
+        
     );
 }
