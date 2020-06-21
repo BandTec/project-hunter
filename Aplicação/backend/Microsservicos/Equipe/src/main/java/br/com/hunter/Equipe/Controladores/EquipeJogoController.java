5fc97dd722b8789b1742e2cf34783ad1e6a092b7
oscar <oscar.casavelha@gmail.com>
2020-06-21 16:50:51 -0300

    rota de pesquisa em Equipe

--- Aplicação/backend/Microsservicos/Equipe/src/main/java/br/com/hunter/Equipe/Controladores/EquipeJogoController.java
+++ Aplicação/backend/Microsservicos/Equipe/src/main/java/br/com/hunter/Equipe/Controladores/EquipeJogoController.java
@@ -1,8 +1,12 @@
 package br.com.hunter.Equipe.Controladores;
 
 
+import br.com.hunter.Equipe.Modelos.Equipe;
 import br.com.hunter.Equipe.Modelos.EquipeJogo;
+import br.com.hunter.Equipe.Modelos.Jogo;
 import br.com.hunter.Equipe.Repositorios.EquipeJogoRepository;
+import br.com.hunter.Equipe.Repositorios.EquipeRepository;
+import br.com.hunter.Equipe.Repositorios.JogoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +19,11 @@ import java.util.Optional;
 @RequestMapping("/equipejogo")
 @CrossOrigin(origins = "http://localhost:3000")
 public class EquipeJogoController {
+
+    @Autowired
+    JogoRepository jogoRepository;
+    @Autowired
+    EquipeRepository equipeRepository;
 
     @Autowired
     private EquipeJogoRepository repository;
@@ -34,6 +43,20 @@ public class EquipeJogoController {
     private ResponseEntity BuscaPorJogo(@PathVariable("equipe") Integer equipe) {
         List<EquipeJogo> lista = repository.findByIdEquipe_IdEquipe(equipe);
         return lista.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(lista);
+    }
+
+    @GetMapping("/busca/{texto}")
+    private ResponseEntity Busca(@PathVariable("texto") String texto) {
+        List<Jogo> listaJogo = jogoRepository.findByNomeJogo(texto);
+        System.out.println("Lista jogo " + listaJogo);
+        if (listaJogo.isEmpty()) {
+            List<Equipe> listaEquipe = equipeRepository.findByNomeEquipe(texto);
+            System.out.println("Lista equipe " + listaEquipe);
+            return listaEquipe.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(listaEquipe);
+        }
+        else{
+            return listaJogo.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(listaJogo);
+        }
     }
 
     @GetMapping("/jogo/{jogo}")
