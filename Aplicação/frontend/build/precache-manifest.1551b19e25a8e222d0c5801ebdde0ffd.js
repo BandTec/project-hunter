44593c7ad46080caf501a4774756f4ad4c36f76c
Henriqe Matos <henriquee.matos@hotmail.com>
2020-06-24 20:37:14 -0300

    frontend nuvem

---
+++ Aplicação/frontend/build/precache-manifest.1551b19e25a8e222d0c5801ebdde0ffd.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44de54a0e1fd9aeac28bd330b578c824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10918d5194d021146457",
+    "url": "/static/css/main.a7c466d5.chunk.css"
+  },
+  {
+    "revision": "ca923a7c03474f1e57de",
+    "url": "/static/js/2.852a28f8.chunk.js"
+  },
+  {
+    "revision": "7799a0448b5eb54147d14e16b3ff760c",
+    "url": "/static/js/2.852a28f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10918d5194d021146457",
+    "url": "/static/js/main.e1d8b5e5.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "270e64feb012eed5d7035c129befe86f",
+    "url": "/static/media/Fernando Caraça Correa-icon.270e64fe.jpg"
+  },
+  {
+    "revision": "270e64feb012eed5d7035c129befe86f",
+    "url": "/static/media/Fernando Correa-icon.270e64fe.jpg"
+  },
+  {
+    "revision": "758838b340e7e8851321ac7dcce1f26a",
+    "url": "/static/media/Henrique Matos-icon.758838b3.jpg"
+  },
+  {
+    "revision": "270e64feb012eed5d7035c129befe86f",
+    "url": "/static/media/Vinícius Oliveira-icon.270e64fe.jpg"
+  },
+  {
+    "revision": "c8c785ff95ab44b9c6a72e4f73583492",
+    "url": "/static/media/camera.c8c785ff.svg"
+  },
+  {
+    "revision": "2d9314a3003f944d04a442efc0f3befc",
+    "url": "/static/media/csgo-icon.2d9314a3.svg"
+  },
+  {
+    "revision": "9a90b732dd496eb292ce15dc3b517c6f",
+    "url": "/static/media/csgo.9a90b732.jpg"
+  },
+  {
+    "revision": "63020cd1900423a5893bb936a8fd5640",
+    "url": "/static/media/dota2-icon.63020cd1.png"
+  },
+  {
+    "revision": "fdc014aacc82bf8959a749659a592dfb",
+    "url": "/static/media/dota2-icon.fdc014aa.svg"
+  },
+  {
+    "revision": "758838b340e7e8851321ac7dcce1f26a",
+    "url": "/static/media/equipe henricao-icon.758838b3.png"
+  },
+  {
+    "revision": "89622c32092df962de18bee5d57b871f",
+    "url": "/static/media/fort.89622c32.jpg"
+  },
+  {
+    "revision": "8ee60e1878120a074b7656d9000233a7",
+    "url": "/static/media/kabum-icon.8ee60e18.png"
+  },
+  {
+    "revision": "ba5bac4f1fc04642635d82a28289b727",
+    "url": "/static/media/kabum-icon.ba5bac4f.svg"
+  },
+  {
+    "revision": "5930cd3ae8e4c5c16c526cdb6419f759",
+    "url": "/static/media/keyd-icon.5930cd3a.png"
+  },
+  {
+    "revision": "ce0868c535e659a30ea92adf7d5c46ca",
+    "url": "/static/media/keyd-icon.ce0868c5.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "78ff4a7a51a4d9b1b764414e2a101031",
+    "url": "/static/media/lol-icon.78ff4a7a.svg"
+  },
+  {
+    "revision": "605811c3aac2096fde24ab3a7b3df85e",
+    "url": "/static/media/lolwp.605811c3.jpg"
+  },
+  {
+    "revision": "270e64feb012eed5d7035c129befe86f",
+    "url": "/static/media/nando2ponto0-icon.270e64fe.jpg"
+  },
+  {
+    "revision": "42ff374eae090f5ff9735e95b3e37a8d",
+    "url": "/static/media/over.42ff374e.jpg"
+  },
+  {
+    "revision": "5b0614e6ba9d79f39d19d4c4b6ddfbb6",
+    "url": "/static/media/overwatch-icon.5b0614e6.svg"
+  },
+  {
+    "revision": "68f8e9841c94bbbcce11597ea3f0a9d1",
+    "url": "/static/media/pain-icon.68f8e984.svg"
+  },
+  {
+    "revision": "ad719afb86b2efb69756a07e9f2c809f",
+    "url": "/static/media/pain-icon.ad719afb.png"
+  },
+  {
+    "revision": "44a2c65b8a60b546cd81852c2b840dcc",
+    "url": "/static/media/team-icon.44a2c65b.svg"
+  },
+  {
+    "revision": "347599642a08e9f711b6b765146598f8",
+    "url": "/static/media/testeLogo.34759964.svg"
+  },
+  {
+    "revision": "14326a4e5e2efa21f5295606bcbb119c",
+    "url": "/static/media/testeLogo3.14326a4e.svg"
+  },
+  {
+    "revision": "a3cc64747201db14ba2fa25cf5974734",
+    "url": "/static/media/testeLogo3.a3cc6474.png"
+  },
+  {
+    "revision": "270e64feb012eed5d7035c129befe86f",
+    "url": "/static/media/time do nando-icon.270e64fe.png"
+  },
+  {
+    "revision": "270e64feb012eed5d7035c129befe86f",
+    "url": "/static/media/user1.270e64fe.jpg"
+  },
+  {
+    "revision": "42fb15a4a6930fc5d81b98dfb1345064",
+    "url": "/static/media/valorant-icon.42fb15a4.png"
+  },
+  {
+    "revision": "49f693879a0736537e32d5391c502cb7",
+    "url": "/static/media/valorant-icon.49f69387.svg"
+  }
+]);
