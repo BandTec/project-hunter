1bc102cb2d966607bed9f2ff08b0c32e0ac2a652
Fernando <fe.c.correa10@gmail.com>
2020-09-04 22:50:53 -0300

    criação de tela de cadastro e recuperação de senha

--- App Mobile/ProjectHunter/app/src/main/java/com/example/projecthunter/RecSenha.kt
+++ App Mobile/ProjectHunter/app/src/main/java/com/example/projecthunter/RecSenha.kt
@@ -1,11 +1,28 @@
 package com.example.projecthunter
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_recsenha.*
 
 class RecSenha  : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recsenha)
     }
+
+    fun recuperarSenha(componente:View) {
+        if (!et_email.text.toString().equals("")) {
+            val email = et_email.text.toString()
+            if (!email.all { it.isLetter() } || !email.equals(" "))
+            {
+                Toast.makeText(this, "Por favor preencha seu email corretamente!", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "Email de recuperação de senha enviado. Verifique seu email.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
